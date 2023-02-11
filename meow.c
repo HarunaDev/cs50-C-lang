67ab9859c319ca6641d1a9060dbbd40a02d12b9c
Alvin Haruna <harunaalvin69@gmail.com>
2023-02-11 15:34:39 +0100

    using while look to repeat task

---
+++ meow.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+// using while loop to repeat task.
+
+int main(void)
+{
+    int counter = 3;
+
+    while (counter > 0)
+    {
+        printf("meow \n");
+        counter--;
+    }
+}
