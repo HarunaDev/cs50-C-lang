0768a6006f2cb28fe80b07b8fce002e4f171d6b9
Alvin Haruna <harunaalvin69@gmail.com>
2023-02-11 18:39:13 +0100

    refactored code using functions

--- mario.c
+++ mario.c
@@ -6,15 +6,31 @@
 int main(void)
 {
     // get size of grid
+    
+    // get size
+    int get_size(void);
+    int size = get_size();
+
+    // print grid 
+    void print_grid(int size);
+    print_grid(size);
+}
+
+
+// get size function
+int get_size(void)
+{
     int size;
-    // using do while loop to check user input is greater than 1
     do 
     {
         size = get_int("Size: ");
     }
     while (size < 1);
+}
 
-    // loop to print required size of bricks onto the console
+// print grid function
+void print_grid(int size)
+{
     for (int i = 0; i < size; i++)
     {
         for (int j = 0; j < size; j++) 
