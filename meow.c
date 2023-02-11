58c35bac4c9f79d37b1675cb6fb70fe0857a2a41
Alvin Haruna <harunaalvin69@gmail.com>
2023-02-11 15:39:50 +0100

    added notes on loop

--- meow.c
+++ meow.c
@@ -11,3 +11,5 @@ int main(void)
         counter--;
     }
 }
+
+// what does it mean to loop or repeat something three times - it basically means to do one task, then count down and do another task till you exit the loop.
